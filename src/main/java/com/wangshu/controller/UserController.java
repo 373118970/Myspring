0271525b0237220e74c21e95812266cf2fbbea21
ZhangYi520 <1073988580@qq.com>
2019-07-22 16:52:34 +0800

    小毅毅提交的代码

--- src/main/java/com/wangshu/controller/UserController.java
+++ src/main/java/com/wangshu/controller/UserController.java
@@ -11,21 +11,22 @@ import javax.servlet.http.HttpServletResponse;
 import javax.xml.registry.infomodel.User;
 import java.io.IOException;
 
+//小毅毅到此一游
 @Controller
 @RequestMapping("/user")
 public class UserController extends HttpServlet {
-//
-//    @Resource
-//    private IUserService userService;
-//
-//    @RequestMapping("/showUser.do")
-//    public void selectUser(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        request.setCharacterEncoding("UTF-8");
-//        response.setCharacterEncoding("UTF-8");
-//        long userId = Long.parseLong(request.getParameter("id"));
-//        User user = this.userService.selectUser(userId);
-//        ObjectMapper mapper = new ObjectMapper();
-//        response.getWriter().write(mapper.writeValueAsString(user));
-//        response.getWriter().close();
-//    }
+
+    @Resource
+    private IUserService userService;
+
+    @RequestMapping("/showUser.do")
+    public void selectUser(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        long userId = Long.parseLong(request.getParameter("id"));
+        User user = this.userService.selectUser(userId);
+        ObjectMapper mapper = new ObjectMapper();
+        response.getWriter().write(mapper.writeValueAsString(user));
+        response.getWriter().close();
+    }
 }
